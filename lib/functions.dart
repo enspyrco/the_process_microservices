ce9fcf26680fb982612e2abece6da61f321e4872
Nick Meinhold <nick.meinhold@gmail.com>
2021-01-23 19:34:05 +1100

    Initial commit.

---
+++ lib/functions.dart
@@ -0,0 +1,8 @@
+import 'package:functions_framework/functions_framework.dart';
+
+import 'package:shelf/shelf.dart';
+
+@CloudFunction()
+Response function(Request request) {
+  return Response.ok('hhelo!');
+}

cdee4bc506338c6a48e8cb08a5031a1991500ab6
Nick Meinhold <nick.meinhold@gmail.com>
2021-01-24 20:27:51 +1100

    partial

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,31 +1,61 @@
+import 'dart:async';
+
 import 'package:functions_framework/functions_framework.dart';
 
 import 'package:shelf/shelf.dart';
+// import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:googleapis/secretmanager/v1.dart';
 
 @CloudFunction()
-Response function(Request request, RequestLogger logger) {
+FutureOr<Response> function(Request request) async {
+  // Extract the query parameters.
+  final codeParam = request.requestedUri.queryParameters['code'];
+  final stateParam = request.requestedUri.queryParameters['state'];
+
+  print('code: $codeParam, state: $stateParam');
+
   // If we can't get both the code and state from the request it's probably an
-  // error message so just send back the original url
-  if (request.requestedUri.queryParameters['code'] == null ||
-      request.requestedUri.queryParameters['state'] == null) {
-    return Response.ok(request.requestedUri);
+  // error message so just send back the original url.
+  if (codeParam == null || stateParam == null) {
+    return Response.ok(request.requestedUri.toString());
   }
 
-  // logger.log(`Exchanging code for tokens...`);
+  final autoRefreshingClient =
+      await clientViaApplicationDefaultCredentials(scopes: []);
 
-  return Response.ok('hhelo!');
+  final secret = Secret()
+    ..name = 'SecretName'
+    ..replication = Automatic();
+
+  final secretManagerApi = SecretmanagerApi(autoRefreshingClient);
+  final createdSecret = await secretManagerApi.projects.secrets
+      .create(secret, 'projects/the-process-tool', secretId: 'foobar');
+  print(createdSecret.name);
+
+  print('Exchanging code for tokens...');
+
+  // Build the post string from an object
+  // final queryParameters = {
+  //   'grant_type': 'authorization_code',
+  //   'client_id': project_credentials.asana.client_id,
+  //   'client_secret': project_credentials.asana.client_secret,
+  //   'redirect_uri': project_credentials.asana.redirect_uri,
+  //   'code': codeParam,
+  // };
+
+  // final uri =
+  //   Uri.https('https://app.asana.com', '/-/oauth_token', queryParameters);
+
+  // final response = await http.post(uri);
+
+  // final resp =
+  //     await http.post( + postData);
+
+  // return Response.seeOther(location);
+
+  return Response.ok('Yo!');
 }
-
-//   // Build the post string from an object
-//   const post_data = querystring.stringify({
-//     'grant_type' : 'authorization_code',
-//     'client_id' : project_credentials.asana.client_id,
-//     'client_secret' : project_credentials.asana.client_secret,
-//     'redirect_uri' : project_credentials.asana.redirect_uri,
-//     'code' : req.query.code,
-//   });
-
-//   const resp = await axios.post('https://app.asana.com/-/oauth_token', post_data);
 
 //   const email = resp.data.data.email;
 
